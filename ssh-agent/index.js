3f3d8113f934a2368d2e2c840b9ed147caca5c47
Jacob See <jacob.d.see@gmail.com>
2020-07-22 00:03:49 -0700

    initial commit ssh-agent action

---
+++ ssh-agent/index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core')
+const process = require('child_process')
+var fs = require('fs')
+
+try {
+  if(!fs.existsSync("~/.ssh")) {
+    fs.mkdirSync("~/.ssh")
+  }
+  console.log("Building known hosts")
+  process.execSync(
+    `ssh-keyscan -p ${core.getInput('ssh_port')} ${core.getInput('domain')} >> ~/.ssh/known_hosts`,
+    {
+      timeout: 20000
+    }
+  )
+  console.log("Finished building known hosts!")
+  console.log("Starting ssh-agent")
+  sshOutput = process.execFileSync(`ssh-agent`, ["-a", core.getInput('ssh_auth_sock')])
+  sshOutput.toString().split("\n").forEach((line) => {
+    var regexp = /=(.*); /g;
+    if (line.includes("SSH_AUTH_SOCK")) {
+      const sock = regexp.exec(line)[1];
+      core.exportVariable("SSH_AUTH_SOCK", sock)
+      console.log(`Agent socket is ${sock}`)
+    }
+    if (line.includes("SSH_AGENT_PID")) {
+      const pid = regexp.exec(line)[1]
+      core.exportVariable("SSH_AGENT_PID", pid)
+      console.log(`Agent PID is ${pid}`)
+    }
+  })
+  console.log("Started ssh-agent")
+  console.log(sshOutput)
+} catch (error) {
+  console.log(error)
+}
