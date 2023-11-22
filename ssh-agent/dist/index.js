2cfdbf708559607ada14215c33f7bd9e192d3f27
Gareth Healy <garethahealy@gmail.com>
2023-11-22 11:33:22 +0000

    🦄🗯️ added ssh build with linting (#121) 🦄🗯️

--- ssh-agent/dist/index.js
+++ ssh-agent/dist/index.js
@@ -2840,55 +2840,73 @@ module.exports = require("util");
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-const core = __nccwpck_require__(855)
-const proc = __nccwpck_require__(81)
-const fs = __nccwpck_require__(147)
+const core = __nccwpck_require__(855);
+const proc = __nccwpck_require__(81);
+const fs = __nccwpck_require__(147);
 
-try {
-  if(!fs.existsSync(`${process.env['HOME']}/.ssh`)) {
-    fs.mkdirSync(`${process.env['HOME']}/.ssh`, {recursive: true})
-  }
-  console.log("Building known hosts")
-  proc.execSync(
-    `ssh-keyscan -p ${core.getInput('ssh_port')} ${core.getInput('domain')} >> ${process.env['HOME']}/.ssh/known_hosts`,
-    {
-      stdio: [null, null, null],
-      timeout: 20000
+function run() {
+    try {
+      if (!fs.existsSync(`${process.env["HOME"]}/.ssh`)) {
+        fs.mkdirSync(`${process.env["HOME"]}/.ssh`, { recursive: true });
+      }
+
+      console.log("Building known hosts");
+
+      proc.execSync(
+        `ssh-keyscan -p ${core.getInput("ssh_port")} ${core.getInput(
+          "domain"
+        )} >> ${process.env["HOME"]}/.ssh/known_hosts`,
+        {
+          stdio: [null, null, null],
+          timeout: 20000,
+        }
+      );
+
+      console.log("Finished building known hosts!");
+      console.log("Starting ssh-agent");
+
+      var sshOutput = proc.execFileSync(`ssh-agent`, [
+        "-a",
+        core.getInput("ssh_auth_sock"),
+      ]);
+
+      sshOutput
+        .toString()
+        .split("\n")
+        .forEach((line) => {
+          var regexp = /=(.*); /g;
+          if (line.includes("SSH_AUTH_SOCK")) {
+            const sock = regexp.exec(line)[1];
+            core.exportVariable("SSH_AUTH_SOCK", sock);
+            console.log(`Agent socket is ${sock}`);
+          }
+
+          if (line.includes("SSH_AGENT_PID")) {
+            const pid = regexp.exec(line)[1];
+            core.exportVariable("SSH_AGENT_PID", pid);
+            console.log(`Agent PID is ${pid}`);
+          }
+        });
+
+      console.log("Exported agent variables");
+      console.log("Started ssh-agent!");
+      console.log("Adding identity");
+
+      proc.execSync("ssh-add -", {
+        stdio: [null, null, null],
+        input: core.getInput("private_key").trim() + "\n",
+      });
+
+      console.log("Added identity!");
+      console.log("ssh-agent is ready to use");
+    } catch (error) {
+      console.log("Encountered an error:");
+      console.log(error.message);
+      process.exit(1);
     }
-  )
-  console.log("Finished building known hosts!")
-  console.log("Starting ssh-agent")
-  sshOutput = proc.execFileSync(`ssh-agent`, ["-a", core.getInput('ssh_auth_sock')])
-  sshOutput.toString().split("\n").forEach((line) => {
-    var regexp = /=(.*); /g;
-    if (line.includes("SSH_AUTH_SOCK")) {
-      const sock = regexp.exec(line)[1];
-      core.exportVariable("SSH_AUTH_SOCK", sock)
-      console.log(`Agent socket is ${sock}`)
-    }
-    if (line.includes("SSH_AGENT_PID")) {
-      const pid = regexp.exec(line)[1]
-      core.exportVariable("SSH_AGENT_PID", pid)
-      console.log(`Agent PID is ${pid}`)
-    }
-  })
-  console.log("Exported agent variables")
-  console.log("Started ssh-agent!")
-  console.log("Adding identity")
-  proc.execSync(
-    "ssh-add -",
-    {
-      stdio: [null, null, null],
-      input: core.getInput("private_key").trim() + "\n"
-    }
-  )
-  console.log("Added identity!")
-  console.log("ssh-agent is ready to use")
-} catch (error) {
-  console.log("Encountered an error:")
-  console.log(error.message)
-  process.exit(1)
 }
+
+run();
 })();
 
 module.exports = __webpack_exports__;
--- ssh-agent/index.js
+++ ssh-agent/index.js
@@ -1,49 +1,67 @@
-const core = require('@actions/core')
-const proc = require('child_process')
-const fs = require('fs')
+const core = require("@actions/core");
+const proc = require("child_process");
+const fs = require("fs");
 
-try {
-  if(!fs.existsSync(`${process.env['HOME']}/.ssh`)) {
-    fs.mkdirSync(`${process.env['HOME']}/.ssh`, {recursive: true})
+function run() {
+  try {
+    if (!fs.existsSync(`${process.env["HOME"]}/.ssh`)) {
+      fs.mkdirSync(`${process.env["HOME"]}/.ssh`, { recursive: true });
+    }
+
+    console.log("Building known hosts");
+
+    proc.execSync(
+      `ssh-keyscan -p ${core.getInput("ssh_port")} ${core.getInput(
+        "domain"
+      )} >> ${process.env["HOME"]}/.ssh/known_hosts`,
+      {
+        stdio: [null, null, null],
+        timeout: 20000,
+      }
+    );
+
+    console.log("Finished building known hosts!");
+    console.log("Starting ssh-agent");
+
+    var sshOutput = proc.execFileSync(`ssh-agent`, [
+      "-a",
+      core.getInput("ssh_auth_sock"),
+    ]);
+
+    sshOutput
+      .toString()
+      .split("\n")
+      .forEach((line) => {
+        var regexp = /=(.*); /g;
+        if (line.includes("SSH_AUTH_SOCK")) {
+          const sock = regexp.exec(line)[1];
+          core.exportVariable("SSH_AUTH_SOCK", sock);
+          console.log(`Agent socket is ${sock}`);
+        }
+
+        if (line.includes("SSH_AGENT_PID")) {
+          const pid = regexp.exec(line)[1];
+          core.exportVariable("SSH_AGENT_PID", pid);
+          console.log(`Agent PID is ${pid}`);
+        }
+      });
+
+    console.log("Exported agent variables");
+    console.log("Started ssh-agent!");
+    console.log("Adding identity");
+
+    proc.execSync("ssh-add -", {
+      stdio: [null, null, null],
+      input: core.getInput("private_key").trim() + "\n",
+    });
+
+    console.log("Added identity!");
+    console.log("ssh-agent is ready to use");
+  } catch (error) {
+    console.log("Encountered an error:");
+    console.log(error.message);
+    process.exit(1);
   }
-  console.log("Building known hosts")
-  proc.execSync(
-    `ssh-keyscan -p ${core.getInput('ssh_port')} ${core.getInput('domain')} >> ${process.env['HOME']}/.ssh/known_hosts`,
-    {
-      stdio: [null, null, null],
-      timeout: 20000
-    }
-  )
-  console.log("Finished building known hosts!")
-  console.log("Starting ssh-agent")
-  sshOutput = proc.execFileSync(`ssh-agent`, ["-a", core.getInput('ssh_auth_sock')])
-  sshOutput.toString().split("\n").forEach((line) => {
-    var regexp = /=(.*); /g;
-    if (line.includes("SSH_AUTH_SOCK")) {
-      const sock = regexp.exec(line)[1];
-      core.exportVariable("SSH_AUTH_SOCK", sock)
-      console.log(`Agent socket is ${sock}`)
-    }
-    if (line.includes("SSH_AGENT_PID")) {
-      const pid = regexp.exec(line)[1]
-      core.exportVariable("SSH_AGENT_PID", pid)
-      console.log(`Agent PID is ${pid}`)
-    }
-  })
-  console.log("Exported agent variables")
-  console.log("Started ssh-agent!")
-  console.log("Adding identity")
-  proc.execSync(
-    "ssh-add -",
-    {
-      stdio: [null, null, null],
-      input: core.getInput("private_key").trim() + "\n"
-    }
-  )
-  console.log("Added identity!")
-  console.log("ssh-agent is ready to use")
-} catch (error) {
-  console.log("Encountered an error:")
-  console.log(error.message)
-  process.exit(1)
 }
+
+run();
